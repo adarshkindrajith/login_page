c31b6582bd1d40efcfb138703f733e4785dd0cf9
Adarsh <adarshkindrajith@gmail.com>
2024-11-27 15:52:49 +0530

    initial commit

---
+++ log_page/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('loginn/', views.loginn,name='loginn'),
+    path('signup/', views.signup,name='signup'),
+    path('home/', views.home,name='home'),
+    
+    
+
+]
