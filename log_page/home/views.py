24bf4bd28b4bf00e49494113cc8fdef4b4e2cdc0
Adarsh <adarshkindrajith@gmail.com>
2024-11-29 17:34:03 +0530

    adding images and update css

--- log_page/home/views.py
+++ log_page/home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages 
-from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 
@@ -12,8 +11,6 @@ def index(request):
     return render(request,'index.html')
 
 
-
-@never_cache
 @login_required(login_url='loginn/')
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def loginn(request):
